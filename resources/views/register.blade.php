1ea93e99ce7d266dcb38ee9c0226f410286a7f4d
lira liska <liraliska09@gmail.com>
2025-06-26 18:58:26 +0700

    pemweb-jumat

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -9,12 +9,19 @@
 <body class="bg-gray-100 flex items-center justify-center h-screen">
 
   <div class="w-full max-w-md bg-white rounded-xl shadow-lg p-8">
-    <h2 class="text-2xl font-bold text-center text-blue-600 mb-6">Welcome Back</h2>
+    <h2 class="text-2xl font-bold text-center text-blue-600 mb-6">Register Account</h2>
     @if (session("error"))
         <p class="text-danger">{{session("error")}}</p>
         @endif
     <form method="POST">
      @csrf
+      <div class="mb-4">
+        <label class="block text-sm mb-1">Name</label>
+        <input type="text" name="name" class="w-full border border-gray-300 rounded-lg px-4 py-2 focus:ring-2 focus:ring-blue-500" placeholder="you@example.com" />
+      </div>
+      @error('name')
+            <p style="color: red">{{$message}}</p>
+      @enderror
       <div class="mb-4">
         <label class="block text-sm mb-1">Email or Username</label>
         <input type="text" name="email" class="w-full border border-gray-300 rounded-lg px-4 py-2 focus:ring-2 focus:ring-blue-500" placeholder="you@example.com" />
@@ -23,12 +30,12 @@
         <label class="block text-sm mb-1">Password</label>
         <input type="password" name="password" class="w-full border border-gray-300 rounded-lg px-4 py-2 focus:ring-2 focus:ring-blue-500" placeholder="********" />
       </div>
-      <button type="submit" class="w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 transition">Login</button>
+      <button type="submit" class="w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 transition">Register</button>
     </form>
 
     <p class="text-sm text-center mt-4 text-gray-500">
-      Don't have an account?
-      <a href="{{ route('register') }}" class="text-blue-600 hover:underline">Sign up</a>
+      Do you have an account?
+      <a href="{{ route('login') }}" class="text-blue-600 hover:underline">Sign in</a>
     </p>
   </div>
 
