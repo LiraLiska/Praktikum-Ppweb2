99e8faf751894d9d4b205ae01e272ef89059d61e
lira liska <liraliska09@gmail.com>
2025-07-11 13:34:53 +0700

    commit

---
+++ pertemuan-2/variabel_user.php
@@ -0,0 +1,11 @@
+<?php
+    //mendefinisikan variabel
+    $nama = "Lira";
+    $umur = 18;
+    $bb = 42;
+
+    echo "Nama saya adalah :" .$nama;
+    echo "<br/> umur :" .$umur. "tahun";
+    echo "<br/> berat badan :" .$bb. "kg";
+    
+?>
